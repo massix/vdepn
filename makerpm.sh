a4c3536040e3bf9e9590de73630bf8747eff1036
Massimo Gengarelli <gengarel@cs.unibo.it>
2011-02-27 12:24:36 +0100

    Add release number to makerpm.sh

--- makerpm.sh
+++ makerpm.sh
@@ -2,18 +2,20 @@
 
 git clean -fx
 
-echo "Version is $1"
+echo "Version is $1-$2"
 
 VERSION=$1
+RELEASE=$2
 
 ./autogen.sh
 ./configure --prefix=/usr
-make DESTDIR=/home/$USER/rpmbuild/BUILDROOT/vdepn-$1-1.i386 install
+make DESTDIR=/home/$USER/rpmbuild/BUILDROOT/vdepn-$1-$2.i386 install
 
 echo "Modifying .spec file"
 
 sed -i "s/Version: [0-9].[0-9].[0-9]/Version: ${VERSION}/" ~/rpmbuild/vdepn.spec
+sed -i "s/Release: [0-9]/Release: ${RELEASE}/" ~/rpmbuild/vdepn.spec
 
 rpmbuild -ba ~/rpmbuild/vdepn.spec
 
-su -c "rpm -Uvh /home/massi/rpmbuild/RPMS/i386/vdepn-${VERSION}-1.i386.rpm"
+su -c "rpm -Uvh /home/massi/rpmbuild/RPMS/i386/vdepn-${VERSION}-${RELEASE}.i386.rpm"
